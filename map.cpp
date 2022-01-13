1c74bff6f78f4f4958c087e4394c524ba7c37b86
Ian Brons <ianebrons@gmail.com>
2022-01-13 14:59:35 -0500

    added vector class

--- map.cpp
+++ map.cpp
@@ -1,1 +1,4 @@
 #include "map.h"
+
+Vector::Vector(int x, int y, int z): x(x), y(y), z(z) {}
+Vector::Vector(const Vector &v): x(v.x), y(v.y), z(v.y) {}
--- map.h
+++ map.h
@@ -2,6 +2,12 @@
 #define __MAP_H
 
 class Vector {
+public:
+    int x;
+    int y;
+    int z;
+    Vector(int x, int y, int z);
+    Vector(const Vector &v);
 };
 
 class World {

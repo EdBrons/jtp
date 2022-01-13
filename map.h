5988d314bc69d05ac3a3e3e9ff0d261e84149c3c
Ian Brons <ianebrons@gmail.com>
2022-01-13 15:01:03 -0500

    added vector operators

--- map.h
+++ map.h
@@ -8,6 +8,12 @@ public:
     int z;
     Vector(int x, int y, int z);
     Vector(const Vector &v);
+    Vector operator + (const Vector &v) {
+        return Vector(x + v.x, y + v.y, z + v.z);
+    }
+    Vector operator - (const Vector &v) {
+        return Vector(x - v.x, y - v.y, z - v.z);
+    }
 };
 
 class World {

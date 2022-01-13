243f707249e927ea730bb18d1d1c31287ba23353
Ian Brons <ianebrons@gmail.com>
2022-01-13 15:10:33 -0500

    added norm

--- map.cpp
+++ map.cpp
@@ -1,7 +1,11 @@
 #include "map.h"
+#include <math.h>
 
 Vector::Vector(float x, float y, float z): x(x), y(y), z(z) {}
 Vector::Vector(const Vector &v): x(v.x), y(v.y), z(v.y) {}
 
+float Vector::norm() {
+    return sqrt( (x * x) + (y * y) + (z * z) );
+}
 
 Rect::Rect(Vector p, Vector b): pos(p), bounds(b) {}
